a941c5d204d382ef1c5d9495028d3e9414604f80
Nate <natehuang0913@gmail.com>
2022-11-19 11:55:15 +0800

    1119-2

---
+++ src/views/NewsView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="news">
+    <h1>This is an about page</h1>
+    <HelloWorld title="緊張"/>
+    <images Link="https://fakeimg.pl/100/"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Images from '@/components/Images.vue'
+
+export default {
+  name: 'NewsView',
+  components: {
+    HelloWorld,
+    Images
+  }
+}
+</script>
